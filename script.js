9f8a795234ce577bd2b2ba324d1d844c1025afdf
CallMeKitsu <89200698+CallMeKitsu@users.noreply.github.com>
2022-03-12 08:48:25 +0000

    added data file, project content

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
-function add() {
-  
+for (const element of data) {
+    
   let project = document.createElement('div')
     project.className = 'project'
 
   let cover = document.createElement('img')
-    cover.src = "assets/cover.jpg"
+    cover.src = element.cover_path
     cover.className = 'project__cover'
   
   let title = document.createElement('h2')
-    title.innerHTML = "Aeronautica"
+    title.innerHTML = element.name
     title.className = 'project__title'
   
   let desc = document.createElement('p')
-    desc.innerHTML = 'Lorem ipsum dolor sit amet, noloran sin morentum a lares ter site.'
+    desc.innerHTML = element.desc
     desc.className = 'project__text'
   
+  /*
   let btn = document.createElement('button')
     btn.onclick = function() {alert("buttonnnn")}
     btn.innerHTML = 'see on Replit'
     btn.className = 'project__btn'
+  */
   
   project.appendChild(cover)
   project.appendChild(title)
   project.appendChild(desc)
-  project.appendChild(btn)
+  // project.appendChild(btn)
   document.querySelector('#projects-wrapper').appendChild(project)
   
 }
+
