56cb385940adbb22f6b922aa3783f3112ad1cca5
CallMeKitsu <89200698+CallMeKitsu@users.noreply.github.com>
2022-04-12 10:34:30 +0000

    application de styleDesc()

--- script.js
+++ script.js
@@ -46,14 +46,7 @@ function main() {
   
   // ================================================================= { HIGHLIGHT } 
   
-  let i = 0
-  
-  setInterval( () => {
-      document.querySelector("body > div.banner > div > p").innerHTML = document.querySelector("body > div.banner > div > p").innerHTML.replace('<pink>', ``).replace('</pink>', ``)
-    document.querySelector("body > div.banner > div > p").innerHTML = document.querySelector("body > div.banner > div > p").innerHTML.replace(subtitles[i], `<pink>${subtitles[i]}</pink>`)
-    i += 1
-    if(i === subtitles.length) i = 0
-  },1000)
+  styleDesc()
   
   doActus(actus, '', '')
   
