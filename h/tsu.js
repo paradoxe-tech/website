83e30f44a1fc5a3a9f43f16cd78325706637488b
CallMeKitsu <89200698+CallMeKitsu@users.noreply.github.com>
2022-04-30 13:59:18 +0000

    Tsu.js V1 (distant better js file)

---
+++ h/tsu.js
@@ -0,0 +1,28 @@
+String.prototype.format = function(...vars) {
+  res = this
+
+  for (const e of vars) {
+    res = res.replace('{}', e)
+  }
+
+  return res
+}
+
+Array.prototype.random = function() {
+  
+  return this[Math.floor(Math.random() * this.length)]
+  
+}
+
+Array.prototype.remove = function(...indexes) {
+
+  i = 0
+  
+  for (var e of indexes) {
+    this.splice(e-i, 1)
+    i += 1
+  }
+  
+  return this
+  
+}
