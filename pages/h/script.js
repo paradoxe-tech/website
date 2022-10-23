68ca9df29fe21203717bebdd7254ef0eb0a0d108
CallMeKitsu <89200698+CallMeKitsu@users.noreply.github.com>
2022-10-23 14:13:22 +0000

    beginning /h/

--- pages/h/script.js
+++ pages/h/script.js
@@ -1,51 +1,24 @@
-styleDesc()
+function get(yourUrl) {
+  var Httpreq = new XMLHttpRequest()
+  Httpreq.open("GET", yourUrl, false)
+  Httpreq.send(null)
+  return Httpreq.responseText
+}
+
+const site_URL = 
+const productions = JSON.parse(get(site_URL + "data/productions.json"))
 
 const folders = ['Développement', 'Études', 'Musique', 'Graphisme']
-const wrapper = document.querySelector('#explorer-wrapper')
+const wrapper = document.querySelector('#container')
 
 for (var folder of folders) {
-  wrapper.innerHTML += createExpLine('folder', folder, "")
   filtered_productions = productions.filter(x => x.type === folder)
   for (var product of filtered_productions) {
-    let folder = document.querySelector(`#folder-${product.type}`)
-    wrapper.innerHTML += createExpLine('product', product.name, product)
+    wrapper.innerHTML += `<div class="item">
+          <img class="item-cover" src="../../cdn/assets/projects/${product.name.toLowerCase()}.png">
+          <ion-icon class="item-type" name="game-controller-outline"></ion-icon>
+          <h2 class="item-name">${product.name}</h2>
+          <span class="item-desc">${product.desc}</span>
+        </div>`
   }
-}
-
-function createExpLine(lineType, name, product) {
-  
-  let desc = product.desc || ""
-  let type = product.fileType || ""
-  let date = product.date || ""
-  let linkto = product.site_path || ""
-  let folder = "into-" + product.type || ""
-  let ident = product.name ? "" : ` id='dir-${name}'` 
-  let isProject = product.project || false
-  let project = ''
-
-  if(isProject) project = `<a href = ${site_URL}/p/${linkto}>📋</a>`
-  
-  return `<div ${product.name ? "" : "style='width: calc(100% - 20px)' "}class="explorer-line ${lineType} ${folder}">
-              <table>
-                <tbody><tr>
-                  ${product.name ? `<td class="nom">${name} ${project}</td>` : `<p onclick="selectFolder('${name}')" style="margin: 0px;margin-top: 5px;margin-left: 10px;"${ident}>[-] ${name}</p>` }
-                  <td class="desc">${md(desc)}</td>
-                  <td class="type"><a href='${linkto}'>${type}</a></td>
-                  <td class="date">${date}</td>
-                </tr>
-              </tbody></table>
-            </div>`
-}
-
-function selectFolder(folder) {
-  document.querySelector(`#dir-${folder}`).innerHTML = plusToMinus(document.querySelector(`#dir-${folder}`).innerHTML)
-  for (var line of document.getElementsByClassName(`into-${folder}`)) {
-    line.classList.toggle('inactive')
-  }
-}
-
-function plusToMinus(string) {
-  if(string.includes('[+]')) return string.replace('[+]', '[-]')
-  else if(string.includes('[-]')) return string.replace('[-]', '[+]')
-  else return string
 }
