6dc1b681c513b790322b02149c1d9924dda05c9d
CallMeKitsu <89200698+CallMeKitsu@users.noreply.github.com>
2022-10-24 09:12:53 +0000

    initialisation /h/

--- pages/h/script.js
+++ pages/h/script.js
@@ -6,19 +6,38 @@ function get(yourUrl) {
 }
 
 const site_URL = `${window.location.origin}/cdn/`
-const productions = JSON.parse(get(site_URL + "data/productions.json"))
+let productions = JSON.parse(get(site_URL + "data/productions.json"))
 
-const folders = ['Développement', 'Études', 'Musique', 'Graphisme']
-const wrapper = document.querySelector('#container')
-
-for (var folder of folders) {
-  filtered_productions = productions.filter(x => x.type === folder)
-  for (var product of filtered_productions) {
-    wrapper.innerHTML += `<div class="item">
-          <img class="item-cover" src="../../cdn/assets/projects/${product.name.toLowerCase()}.png">
-          <ion-icon class="item-type" name="game-controller-outline"></ion-icon>
-          <h2 class="item-name">${product.name}</h2>
-          <span class="item-desc">${product.desc}</span>
-        </div>`
+function sortByDate(a, b) {
+  
+  let date_a = new Date(a.date).getTime()
+  let date_b = new Date(b.date).getTime()
+  
+  if (date_a < date_b) {
+    return -1
   }
+  if (date_a > date_b) {
+    return 1
+  }
+  
+  return 0
+}
+
+productions = productions.sort(sortByDate).reverse()
+
+console.log(productions)
+
+for (var product of productions) {
+
+  let year = product.date.split('-')[0]
+  let month = product.date.split('-')[1] ? `${product.date.split('-')[1]}/` : ""
+  let day = product.date.split('-')[2] ? `${product.date.split('-')[2]}/` : ""
+  
+  document.querySelector('#container').innerHTML += `<div class="item">
+      <img class="item-cover" src="../../cdn/assets/projects/${product.delta}.png">
+      <ion-icon class="item-type" name="${product.type}"></ion-icon>
+      <span class="item-date">${day}${month}${year}</span>
+      <h2 class="item-name">${product.name}</h2>
+      <span class="item-desc">${md(product.desc)}</span>
+    </div>`
 }
