8724d570f278f9e394143400d6e1f2906f9400ff
CallMeKitsu <89200698+CallMeKitsu@users.noreply.github.com>
2022-10-25 16:38:22 +0000

    redirections management

--- index.js
+++ index.js
@@ -12,4 +12,16 @@ app.use('/', express.static('home'))
 app.use('/cdn', express.static('cdn'))
 app.use('/', express.static('pages'))
 
+let redirects = [
+  ["/h/paleossena", "paleossena"],
+  ["h/gate", "gate-serv"],
+  ["h/graphsandbox", "graphsandbox"]
+]
+
+app.get("/h/paleossena", (req, res) => res.redirect(`https://paleossena.kitsuforyou.repl.co`))
+app.get("/h/gate", (req, res) => res.redirect(`https://gate-serv.kitsuforyou.repl.co`))
+app.get("/h/graphsandbox", (req, res) => res.redirect(`https://graphsandbox.kitsuforyou.repl.co`))
+app.get("/h/kawan", (req, res) => res.redirect(`https://github.com/callmekitsu/kawan`))
+app.get("/h/tsu-js", (req, res) => res.redirect(`https://callmekitsu.github.io/tsu.js`))
+
 app.listen(8080)
