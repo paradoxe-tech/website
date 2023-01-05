61462395238e826cd5c667d2b7d6a22f2aa4d441
CallMeKitsu <89200698+CallMeKitsu@users.noreply.github.com>
2023-01-05 09:55:28 +0000

    dynamic embedding of /h

--- index.js
+++ index.js
@@ -12,15 +12,43 @@ app.use('/', express.static('home'))
 app.use('/cdn', express.static('cdn'))
 app.use('/', express.static('pages'))
 
-app.get("/h/paleossena", (req, res) => res.redirect(`https://paleossena.kitsuforyou.repl.co`))
-app.get("/h/gate", (req, res) => res.redirect(`https://gate-serv.kitsuforyou.repl.co`))
-app.get("/h/graphsandbox", (req, res) => res.redirect(`https://graphsandbox.kitsuforyou.repl.co`))
-app.get("/h/kawan", (req, res) => res.redirect(`https://github.com/callmekitsu/kawan`))
-app.get("/h/tsu-js", (req, res) => res.redirect(`https://callmekitsu.github.io/tsu.js`))
-app.get("/h/weapons", (req, res) => res.redirect(`https://weapons.kitsuforyou.repl.co`))
-app.get("/h/alterheart", (req, res) => res.redirect(`https://alterheart.kitsuforyou.repl.co`))
-app.get("/h/DRPC", (req, res) => res.redirect(`https://github.com/CallMeKitsu/DiscordRPC/releases`))
-app.get("/h/uni", (req, res) => res.redirect(`https://uni.kitsuforyou.repl.co/create`))
+let embeds = new Map()
+embeds.set("vanta", "https://vanta.kitsuforyou.repl.co")
+embeds.set("paleossena", "https://paleossena.kitsuforyou.repl.co")
+embeds.set("gate", "https://gate-serv.kitsuforyou.repl.co")
+embeds.set("graphsandbox", "https://graphsandbox.kitsuforyou.repl.co")
+embeds.set("kawan", "https://github.com/callmekitsu/kawan")
+embeds.set("tsu-js", "https://callmekitsu.github.io/tsu.js")
+embeds.set("weapons", "https://weapons.kitsuforyou.repl.co")
+embeds.set("alterheart", "https://alterheart.kitsuforyou.repl.co")
+embeds.set("DRPC", "https://github.com/CallMeKitsu/DiscordRPC/releases")
+embeds.set("uni", "https://uni.kitsuforyou.repl.co/create")
+embeds.set("tuuupper", "https://paleossena.kitsuforyou.repl.co")
+
+for(let host of embeds.keys()) {
+  app.use(`/h/${host}`, (req, res) => {
+    let rawHTML = `<!DOCTYPE html>
+        <html>
+          <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width">
+            <title>h/${host} | CMK.</title>
+            <link href="../style.css" rel="stylesheet" type="text/css" />
+          </head>
+          <body style='margin: 0px;overflow: hidden;'>
+             <div> 
+            <object type="text/html" data="${embeds.get(host)}" 
+            style="height: 100vh;width: 100%;overflow:auto;">
+            </object>
+         </div>
+        
+          </body>
+        </html>`
+
+    res.set('Content-Type', 'text/html');
+    res.send(Buffer.from(rawHTML));
+  })
+}
 
 app.get("/discord", (req, res) => res.redirect(`https://discord.gg/wxnsMRVqwa`))
 
